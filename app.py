72ca1df3f2744b7a84d4f1e8a55d6bd2db4bffcd
lakshh670 <er.lakshyasharma2005@gmail.com>
2025-10-14 21:07:43 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import pickle
+movies=pickle.load(open('movies_dict.pkl','rb'))
+similarity=pickle.load(open('similarity.pkl','rb'))
+df=pd.DataFrame(movies)
+
+import requests
+
+def fetch_poster(movie_name):
+    api_key = '965cc9eceaef8a74325bfa6502bddbb1'
+    try:
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={movie_name}"
+        data = requests.get(url).json()
+        if data.get('results'):
+            poster_path = data['results'][0].get('poster_path')
+            if poster_path:
+                return f"https://image.tmdb.org/t/p/w500{poster_path}"
+        return "https://via.placeholder.com/500x750?text=No+Image"
+    except Exception as e:
+        print("Error fetching poster:", e)
+        return "https://via.placeholder.com/500x750?text=Error"
+
+
+
+def recommend(movie):
+    # get the integer index instead of Index object
+    index = df[df['title'] == movie].index[0]  # Remember to put [0] after index
+
+    # enumerate similarity scores
+    distances = list(enumerate(similarity[index]))
+
+    # sort by similarity (highest first) and skip the first (itself)
+    movies_list = sorted(distances, reverse=True, key=lambda x: x[1])[1:6]
+    recommandation,posters=[],[]
+    for i in movies_list:
+        recommandation.append(df.iloc[i[0]].title)
+        posters.append(fetch_poster(df.iloc[i[0]].title))
+    return recommandation,posters
+
+
+
+st.title('🎥 Movie Recommender System')
+
+selected_name = st.selectbox('Select a movie from the dropdown', df['title'].values)
+
+if st.button("Recommend"):
+    names, posters = recommend(selected_name)
+
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
+
